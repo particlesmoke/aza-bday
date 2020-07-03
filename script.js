28ee58efe0f4b8787af1937fe249973b6fba2beb
particlesmoke <f20180468@pilani.bits-pilani.ac.in>
2020-07-03 22:37:15 +0530

    ok

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("count").innerHTML="!!!";
+
+setInterval(function(){
+
+var countDownDate = new Date("Aug 4, 2020 00:00:00").getTime();
+
+var now = new Date().getTime();
+
+var distance = countDownDate - now;
+
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+document.getElementById("count").innerHTML = days + "d hell " + hours + "h " + minutes + "m " + seconds + "s ";
+
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("count").innerHTML = "Yayyy!";
+}
+
+}, 1000);
+

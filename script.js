aadc1d76eb0347880fb10ddb145c4b670c63df50
particlesmoke <f20180468@pilani.bits-pilani.ac.in>
2020-07-06 20:12:39 +0530

    ok

--- script.js
+++ script.js
@@ -13,6 +13,23 @@ var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+if(hours<=1 || hours>=18){
+    document.body.style.backgroundImage = "url(BGn.jpg)";
+    document.getElementById("p2").style.backgroundImage = "linear-gradient(to bottom right, rgba(17, 145, 204, 0.87), rgba(27, 0, 100, 0.651))";
+}
+else if(hours<=19 && hours>=13){
+    document.body.style.backgroundImage = "url(BGm.jpg)";
+    document.getElementById("p2").style.backgroundImage = "linear-gradient(to bottom right, rgba(74, 183, 255, 0.9), rgba(255, 247, 129, 0.6))";
+}
+else if(hours<=12 && hours>=8){
+    document.body.style.backgroundImage = "url(BGa.jpg)";
+    document.getElementById("p2").style.backgroundImage = "linear-gradient(to bottom right, rgba(192, 87, 45, 0.651), rgba(255, 214, 34, 0.87))";
+}
+else{
+    document.body.style.backgroundImage = "url(BGe.jpg";
+    document.getElementById("p2").style.backgroundImage = "linear-gradient(to bottom right, rgba(98, 33, 219, 0.8), rgba(221, 27, 179, 0.712))";
+}
+
 document.getElementById("count").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
 if (distance < 0) {
@@ -21,6 +38,7 @@ if (distance < 0) {
 }
 
 }, 1000);
+
 
 player = document.getElementById("player"); 
 function playmusic()  
